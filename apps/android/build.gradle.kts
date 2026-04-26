cadff2dfe93707f1a1811f45424930ded00e6913
Franco N. Colaizzi <fnc314@gmail.com>
2026-04-26 06:10:07 -0400

    android block in apps:android

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -1,35 +1,57 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
   alias(libs.plugins.tools.gradle.plugins.kmp.app.android)
 }
 
-androidComponents {
-  finalizeDsl { dsl ->
-    dsl.namespace = "com.fnc314.kmp.app.android"
-    dsl.enableKotlin = true
-    dsl.buildFeatures {
-      compose = true
-      resValues = true
+kotlin {
+  jvmToolchain(
+    libs.versions.jdk.map { it.toInt() }.get()
+  )
+  target {
+    compilations.configureEach {
+      compileTaskProvider.configure {
+        compilerOptions {
+          jvmTarget.set(libs.versions.jdk.map { JvmTarget.fromTarget(it) })
+          languageVersion.set(
+            libs.versions.kotlin.map { KotlinVersion.fromVersion(it.substringBeforeLast(".")) }
+          )
+          apiVersion.set(
+            libs.versions.kotlin.map { KotlinVersion.fromVersion(it.substringBeforeLast(".")) }
+          )
+        }
+      }
     }
-    dsl.dependenciesInfo {
-      includeInApk = false
-      includeInBundle = false
-    }
-    dsl.composeOptions {
-      kotlinCompilerExtensionVersion = null
-    }
-    dsl.compileSdk {
-      version = release(libs.versions.android.sdk.compile.map { it.toInt() }.get())
-    }
-    dsl.defaultConfig {
-      applicationId = "com.fnc314.kmp.app.android"
-      versionCode = 1
-      versionName = "1.0"
-    }
-    dsl.compileOptions {
-      isCoreLibraryDesugaringEnabled = true
-      sourceCompatibility(JavaVersion.VERSION_17)
-      targetCompatibility(JavaVersion.VERSION_17)
-    }
+  }
+}
+
+android {
+  namespace = "com.fnc314.kmp.app.android"
+  enableKotlin = true
+  buildFeatures {
+    compose = true
+    resValues = true
+  }
+  dependenciesInfo {
+    includeInApk = false
+    includeInBundle = false
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = null
+  }
+  compileOptions {
+    isCoreLibraryDesugaringEnabled = true
+    sourceCompatibility(JavaVersion.VERSION_17)
+    targetCompatibility(JavaVersion.VERSION_17)
+  }
+  compileSdk {
+    version = release(libs.versions.android.sdk.compile.map { it.toInt() }.get())
+  }
+  defaultConfig {
+    applicationId = "com.fnc314.kmp.app.android"
+    versionCode = 1
+    versionName = "1.0"
   }
 }
 
